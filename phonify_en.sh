48f10c46c4498db5ffd017a8c42ea2f3612c199b
Bogdan Yakupov <yourinfinityuser@gmail.com>
2024-05-21 23:48:08 +0500

    moved voice onnx files to piper_presets

--- phonify_en.sh
+++ phonify_en.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-PIPER_PATH="./piper/piper"
+PIPER_PATH="./piper"
+PIPER_PRESETS_PATH="./piper_presets"
 OUTPUT_PATH="./storage"
 
 echo $2 | $PIPER_PATH/build/piper --espeak-data $PIPER_PATH/install/espeak-ng-data \
---model $PIPER_PATH/build/en_US-ryan-high.onnx \
+--model $PIPER_PRESETS_PATH/en_US-ryan-high.onnx \
 --output_file $OUTPUT_PATH/$1.wav
--- phonify_ru.sh
+++ phonify_ru.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-PIPER_PATH="./piper/piper"
+PIPER_PATH="./piper"
+PIPER_PRESETS_PATH="./piper_presets"
 OUTPUT_PATH="./storage"
 
 echo $2 | $PIPER_PATH/build/piper --espeak-data $PIPER_PATH/install/espeak-ng-data \
---model $PIPER_PATH/build/ru_RU-dmitri-medium.onnx \
+--model $PIPER_PRESETS_PATH/ru_RU-dmitri-medium.onnx \
 --output_file $OUTPUT_PATH/$1.wav
